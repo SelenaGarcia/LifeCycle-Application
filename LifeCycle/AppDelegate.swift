dcef0a8151ab7c23804ef0b3c477b82c581689e8
SelenaGarcia <selenagarcialobo@gmail.com>
2022-07-25 14:36:26 -0300

    life Cycle

--- LifeCycle/AppDelegate.swift
+++ LifeCycle/AppDelegate.swift
@@ -13,7 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //llamado cuando se inicia la aplicación. En el momento en que se llama al método, nuestra aplicación está en estado inactivo pero ya ha cargado la escena gráfica principal.
+        //Además, la restauración del estado (restauración de la jerarquía de la pantalla y el estado de la interfaz de usuario desde la última vez que un usuario usó la aplicación) aún no se ha iniciado. Tiene el userInfo parámetro que podemos usar para determinar el motivo por el cual se lanzó la aplicación.
+        //Por ejemplo, si la aplicación se inició para abrir un documento en una URL específica, es posible que deseemos evitar que se produzca la restauración del estado.
+
+        print(#function)
+
         return true
     }
 
@@ -22,6 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+
+        print(#function)
+
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
@@ -29,6 +37,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+
+       // recibe una llamada cuando se inició nuestra aplicación y el estado de la aplicación se restauró si es necesario.
+       //Sin embargo, la interfaz de usuario aún no se muestra. Al igual que el método anterior, también tiene el parámetro userInfo.
+       //Como señala Apple, esta es la última oportunidad de realizar una lógica relevante para esa propiedad.
+        print(#function)
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+        //llamadas en aplicaciones que no admiten escenas.
+        //De lo contrario, se reemplaza por su covariante sceneWillEnterForeground(_:).
+        //Se llama al método cuando la aplicación pasa a un estado activo desde el fondo.
+        print(#function)
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        //llamadas en aplicaciones que no admiten escenas.
+        //De lo contrario, se reemplaza por su covariante sceneDidBecomeActive(_:) en SceneDelegate.
+        //El método se llama cuando la aplicación ha entrado en el estado activo.
+        //En este momento, la interfaz de usuario se ha cargado, pero aún no se muestra.
+
+        print(#function)
+
+
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+        //llamadas en aplicaciones que no admiten escenas.
+        //De lo contrario, se reemplaza por su covariante sceneWillResignActive(_:) en SceneDelegate.
+        //Se llama al método cuando la aplicación es interrumpida por alertas del sistema, llamadas telefónicas, etc.
+        //También lo activamos cuando descartamos la aplicación con el botón Inicio. Podemos usar este método para preservar los datos no guardados.
+        //Por ejemplo, es posible que queramos guardar la lista de elementos en el disco para que, si el sistema elimina la aplicación o forzamos el cierre, la próxima vez que la abramos podamos ver todos los elementos nuevamente.
+
+        print(#function)
+
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+        //llamadas en aplicaciones que no admiten escenas.
+        //De lo contrario, se reemplaza por su covariante sceneDidEnterBackground(_:) en SceneDelegate.
+        //El método se llama cuando movemos la aplicación al fondo. Aquí, debemos asegurarnos de detener los temporizadores, reducir el uso de la memoria de la aplicación y, si es necesario, prepararnos para la restauración del estado en caso de que la aplicación finalice.
+        //Además, después de que este método regrese, UIKit crea una instantánea de la última interfaz de usuario de la aplicación para mostrarla en el selector de aplicaciones. Por lo tanto, debemos ocultar la información confidencial del usuario de la interfaz de usuario, como las contraseñas.
+
+        print(#function)
+
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        //se llama a este método cuando la aplicación está a punto de cerrarse y eliminarse de la memoria.
+        //Siempre se llama para las aplicaciones que no admiten el modo de fondo. Sin embargo, para las aplicaciones que tienen capacidad en segundo plano, el método generalmente no se llama ya que la aplicación pasa al estado de segundo plano.
+        //Por otro lado, si el sistema decide liberar su memoria y elimina la aplicación que está en segundo plano, se llamará a este método.
+
+        print(#function)
+
     }
 
 
--- LifeCycle/SceneDelegate.swift
+++ LifeCycle/SceneDelegate.swift
@@ -16,6 +16,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+
+        //Se llama cuando la aplicación solicita la interfaz de usuario.
+        //Como se explica en la documentación de Escenas (https://developer.apple.com/documentation/uikit/app_and_environment/scenes), una escena contiene una ventana y controladores de vista.
+        //Básicamente, este método se llama cuando agregamos una escena a la aplicación. La mayoría de las aplicaciones solo tienen una escena, por lo que este método se llamará una vez en tales casos.
+        //Dentro del método, configuramos la jerarquía y establecemos el controlador de vista raíz para la ventana.
+
+        print(#function)
+
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
@@ -24,27 +33,52 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+
+        //se llama cuando un usuario elimina la escena de la aplicación cerrándola en el selector de aplicaciones. El sistema también puede desconectar una escena para liberar espacio en la memoria si es necesario
+        print(#function)
+
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+
+
+        //Como a applicationDidBecomeActive(_:), se llama al método cuando la interfaz de usuario ha entrado en el modo activo en primer plano y la interfaz de usuario se ha cargado, pero aún no se muestra
+
+        print(#function)
+
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+
+        //Al igual que su aplicación covariante sin escena WillResignActive(_:), se llama al método cuando la aplicación es interrumpida por una llamada telefónica o una alerta del sistema. También lo activamos tocando el botón Inicio.
+
+        print(#function)
+
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+
+        // Al igual que su contraparte que no es de escena, applicationWillEnterForeground(_:), se llama al método cuando la aplicación está en un estado inactivo en primer plano y se está preparando para mostrársela al usuario.
+
+        print(#function)
+
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        //Llamada cuando la aplicación ya no está en la pantalla. Como sugiere Apple, podemos usar este método para reducir el uso de memoria de una escena y ocultar información confidencial del usuario de la pantalla, ya que después de que este método regrese, UIKit creará una instantánea de la interfaz de usuario y la mostrará en el selector de aplicaciones.
+
+        print(#function)
+
     }
 
 
