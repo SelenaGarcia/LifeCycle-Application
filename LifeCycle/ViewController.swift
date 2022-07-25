3bfea61ed4e9ebbad1dd5d999c618f33d5cb5313
SelenaGarcia <selenagarcialobo@gmail.com>
2022-07-25 12:15:34 -0300

    Initial Commit

---
+++ LifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Selena Belén Garcia Lobo on 25/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
